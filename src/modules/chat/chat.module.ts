e9d8b7655375a0e4b593d34899e41976c6afc025
zhengkai <zhengkai@gmail.com>
2025-02-15 02:31:26 +0800

    ChatProviderFactory

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { ChatProviderFactory } from './providers/chat-provider.factory';
 
 @Module({
   imports: [HttpModule],
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatProviderFactory],
 })
 export class ChatModule {}
